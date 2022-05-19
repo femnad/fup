08a1d78e6d51c0a25560a1457c1bed5d4dc65588
fcd <git@fcd.dev>
2022-05-19 01:56:37 +0100

    don't unintentionaly create templates as root

--- tasks/templates.py
+++ tasks/templates.py
@@ -10,8 +10,6 @@ TEMPLATED_FILES_SUFFIX = 'files'
 
 def template_file(template: Template, should_sudo):
     src = f'{TEMPLATED_FILES_SUFFIX}/{template.src}'
-    dest = os.path.expanduser(template.dest)
-
     context = {k: os.getenv(v) for k, v in template.context.items()}
 
     files.template(src=src, dest=dest, mode=FILE_PERMISSIONS, _sudo=should_sudo, **context)
@@ -22,5 +20,7 @@ def run(config):
 
     for template in config.templates:
         template = Template(**template)
+        template.dest = os.path.expanduser(template.dest)
         should_sudo = not template.dest.startswith(home)
+
         template_file(template, should_sudo)
