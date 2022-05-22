d2ef6449f417e7d3cd3665372fbfda9a1258443c
fcd <git@fcd.dev>
2022-05-23 00:29:45 +0100

    clone self repos when ssh ready

--- tasks/clone_repos.py
+++ tasks/clone_repos.py
@@ -1,21 +1,32 @@
 import os
 from typing import List
 
-from tasks.config import Repo, Settings
-import tasks.when
-
+from pyinfra import host
 from pyinfra.operations import git
 from pyinfra.api import operation
 
+import facts.base
+import tasks.config
+import tasks.when
 
-def get_clone_dir(repo: Repo, settings: Settings):
+
+def is_self_repo(repo: tasks.config.Repo, settings: tasks.config.Settings) -> bool:
+    repo_owner, _ = repo.name.split('/')[-2:]
+    return repo_owner == settings.github_user
+
+
+def is_self_repo_ready():
+    return host.get_fact(facts.base.SshReady)
+
+
+def get_clone_dir(repo: tasks.config.Repo, settings: tasks.config.Settings):
     repo_owner, repo_name = repo.name.split('/')[-2:]
     base_dir = settings.self_clone_dir if repo_owner == settings.github_user else settings.clone_dir
 
     return os.path.expanduser(f'{base_dir}/{repo_name}')
 
 
-def get_clone_url(repo: Repo, settings: Settings):
+def get_clone_url(repo: tasks.config.Repo, settings: tasks.config.Settings):
     repo_owner, _ = repo.name.split('/')[-2:]
 
     if repo_owner == settings.github_user:
@@ -25,9 +36,12 @@ def get_clone_url(repo: Repo, settings: Settings):
 
 
 @operation
-def clone_repos(repos: List[Repo], settings: Settings):
+def clone_repos(repos: List[tasks.config.Repo], settings: tasks.config.Settings):
     for repo in repos:
         if not tasks.when.should_run(repo.when):
+            continue
+
+        if is_self_repo(repo, settings) and not is_self_repo_ready():
             continue
 
         clone_dir = get_clone_dir(repo, settings)
@@ -37,5 +51,7 @@ def clone_repos(repos: List[Repo], settings: Settings):
 
 
 def run(config):
-    repos = [Repo(**repo) for repo in config.repos]
+    repos = [tasks.config.Repo(**repo) for repo in config.repos]
+    self_repos = [tasks.config.Repo(**repo) for repo in config.repos]
     clone_repos(repos, config.settings)
+    clone_repos(self_repos, config.settings)
