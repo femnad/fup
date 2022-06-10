26e987e4899246ce93a26dd6097eda5fabeb4374
fcd <git@fcd.dev>
2022-06-10 21:09:43 +0100

    account for fully qualified go package names

--- tasks/gopkg.py
+++ tasks/gopkg.py
@@ -6,16 +6,26 @@ from tasks.archives import get_unless
 from tasks.config import GoPkg
 
 
+def get_pkg_name(go_pkg: GoPkg) -> str:
+    name = go_pkg.name
+    fields = name.split('/')
+
+    if '.' in fields[0]:
+        return name
+
+    return f'{go_pkg.host}/{name}'
+
+
 def install_pkg(go_pkg: GoPkg):
-    pkg = f'{go_pkg.host}/{go_pkg.name}@{go_pkg.version}'
+    name = get_pkg_name(go_pkg)
+    pkg = f'{name}@{go_pkg.version}'
     yield StringCommand(f'go install {pkg}')
 
 
 @operation
 def install_pkgs(go_pkgs: List[GoPkg]):
     for pkg in go_pkgs:
-        unless = get_unless(pkg.unless)
-        if not unless.should_proceed():
+        if (unless := get_unless(pkg.unless)) and not unless.should_proceed():
             continue
 
         yield from install_pkg(pkg)
