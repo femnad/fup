a8e8a71f5ef2c6d383515f4a9884c6d1d457ec07
fcd <git@fcd.dev>
2022-11-14 22:37:46 +0000

    log to a file

--- internal/logging.go
+++ internal/logging.go
@@ -1,20 +1,40 @@
 package internal
 
 import (
-	"github.com/op/go-logging"
+	"log"
 	"os"
+	"path"
+
+	"github.com/op/go-logging"
 )
 
 var Log = logging.MustGetLogger("fup")
 
-func InitLogging(level int) {
+func InitLogging(level int, logFile string) {
+	dirName, _ := path.Split(logFile)
+	_, err := os.Stat(dirName)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(dirName, 0755)
+		if err != nil {
+			log.Fatalf("error creating directory %s: %v", dirName, err)
+		}
+	} else if err != nil {
+		log.Fatalf("error checking logging directory %s: %v", dirName, err)
+	}
+
+	f, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("error opening log file: %v", err)
+	}
+
 	format := logging.MustStringFormatter(
 		`%{color}%{time:2006-01-02 15:04:05} %{level:.6s} %{shortfunc} %{message} %{color:reset}`,
 	)
-	stderrLogs := logging.NewLogBackend(os.Stderr, "", 0)
-	formattedLogs := logging.NewBackendFormatter(stderrLogs, format)
 
+	backendLogs := logging.NewLogBackend(f, "", 0)
+	formattedLogs := logging.NewBackendFormatter(backendLogs, format)
 	leveledLogs := logging.AddModuleLevel(formattedLogs)
+
 	leveledLogs.SetLevel(logging.Level(level), "")
 	logging.SetBackend(leveledLogs)
 }
--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"github.com/alexflint/go-arg"
+
 	"github.com/femnad/fup/base"
 	"github.com/femnad/fup/internal"
 	"github.com/femnad/fup/provision"
-	"log"
 )
 
 type args struct {
 	File     string `arg:"-f,--file" default:"~/.config/fup/fup.yml"`
+	LogFile  string `arg:"--logfile" default:"~/.local/share/fup/fup.log"`
 	LogLevel int    `arg:"-l,--loglevel" default:"4"`
 }
 
 func (args) Version() string {
-    return "fup 0.1.0"
+	return "fup 0.1.0"
 }
 
 func main() {
-    var args args
+	var args args
 	arg.MustParse(&args)
-	internal.InitLogging(args.LogLevel)
+
+	logFile := internal.ExpandUser(args.LogFile)
+	internal.InitLogging(args.LogLevel, logFile)
+
 	config, err := base.ReadConfig(args.File)
 	if err != nil {
 		log.Fatalf("%v\n", err)
