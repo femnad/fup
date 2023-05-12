d83a42cfcf1362e1e6aaef6b7fc665bcdade4d3c
fcd <git@fcd.dev>
2023-05-12 04:34:01 +0100

    cleanup python files

--- tasks/preflight.py
+++
@@ -1,7 +0,0 @@
-from tasks.recipes import Recipe, run_recipe
-
-
-def run(config):
-    for recipe in config.preflight:
-        recipe = Recipe(**recipe)
-        run_recipe(recipe, config.settings)
