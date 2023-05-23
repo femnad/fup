d3d391253378ceb824095b807dda9885f8685aea
fcd <git@fcd.dev>
2023-05-23 01:54:05 +0100

    invert the log flag

--- main.go
+++ main.go
@@ -21,9 +21,9 @@ type args struct {
 	DebugToStderr bool     `arg:"-b,--debug-to-stderr" help:"Write logs to stderr"`
 	File          string   `arg:"-f,--file,env:FUP_CONFIG" default:"~/.config/fup/fup.yml" help:"Config file path"`
 	LogFile       string   `arg:"--logfile" default:"~/.local/share/fup/fup.log" help:"Log file path"`
-	LogLevel      int      `arg:"-l,--loglevel" default:"5" help:"Log level as integer"`
+	LogLevel      int      `arg:"-l,--loglevel" default:"5" help:"Log level as integer, 0 least, 5 most"`
+	NoLogs        bool     `arg:"-n,--nologs" help:"Don't write logs to a file"`
 	Provisioners  []string `arg:"-p,--provisioners" help:"List of provisioners to run"`
-	WriteLogs     bool     `arg:"-w,--writelogs" help:"Write logs to a file"`
 }
 
 func (args) Version() string {
@@ -53,7 +53,7 @@ func main() {
 	arg.MustParse(&parsed)
 
 	logFile := internal.ExpandUser(parsed.LogFile)
-	if !parsed.WriteLogs {
+	if parsed.NoLogs {
 		logFile = ""
 	}
 	internal.InitLogging(parsed.LogLevel, logFile, parsed.DebugToStderr)
